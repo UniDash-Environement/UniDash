eb3ec81e4d77bda91dd986989ab3b5f66795b645
Pikatsuto <pikatsuto@gmail.com>
2024-03-05 00:43:32 +0100

    feat: start menu element

---
+++ components/Menu/Left/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="menuLeft">
+
+</div>
+</template>
+
+<style scoped lang="scss">
+.menuLeft {
+  background-color: rgba(0, 173, 181, 0.25);
+  max-height: 100000px;
+  min-height: 100%;
+  width: 280px;
+  box-shadow: 2px 0 2px 0 rgba(0, 173, 181, 0.25);
+
+  z-index: 1001;
+}
+</style>
---
+++ components/Menu/Top/index.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="menuTop">
+
+  </div>
+  <div class="menuTopMargin"></div>
+</template>
+
+<style scoped lang="scss">
+.menuTop {
+  background-color: rgba(57, 62, 70, 0.4);
+  height: 38px;
+  width: calc( 100vw - 280px);
+  box-shadow: 2px 2px 2px 0 rgba(238, 238, 238, 0.25);
+
+  position: fixed;
+  z-index: 1000;
+}
+
+.menuTopMargin {
+  height: 38px;
+}
+</style>
