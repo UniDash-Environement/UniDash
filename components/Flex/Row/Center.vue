38b3d2f57a971b21b321e3a455ac41f10319b2a2
Pikatsuto <pikatsuto@gmail.com>
2024-03-05 00:43:14 +0100

    feat: flex element

---
+++ components/Flex/Row/Center.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="flex">
+  <slot />
+</div>
+</template>
+
+<style scoped lang="scss">
+.flex {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+}
+</style>
---
+++ components/Flex/Row/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="flex">
+  <slot />
+</div>
+</template>
+
+<style scoped lang="scss">
+.flex {
+  display: flex;
+  flex-direction: row;
+}
+</style>
